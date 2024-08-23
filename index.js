e358012195f686cb84693c644a841543380e6b3b
amrithgold <goldandgold1002@gmail.com>
2024-08-23 19:55:37 +0530

    updated index

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const routes = require('./src/route');
 const cors = require('cors')
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+    origin: ['https://amrith-gold.netlify.app', 'http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());  
 const port = process.env.PORT || 3001;
 
