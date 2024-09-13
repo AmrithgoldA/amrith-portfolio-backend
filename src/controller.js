66bd5d1aa84dfb77c2e9e983643e47e2fc32a9a8
amrithgold <goldandgold1002@gmail.com>
2024-09-13 18:33:33 +0530

    updated carrier history route

--- src/controller.js
+++ src/controller.js
@@ -50,9 +50,21 @@ async function getProjectDetails(req, res) {
     }
 }
 
+async function getCarrierDetails(req, res) {
+    try {
+        const response = await client.query('SELECT * FROM JobExperience')
+        res.json(response.rows);
+    }
+    catch (error) {
+        console.error('Error fetching social links');
+        res.status(500).send('Internal Server Error');
+    }
+}
+
 module.exports = {
     getUserSkills,
     getSocialLinks,
     callMailer,
-    getProjectDetails
+    getProjectDetails,
+    getCarrierDetails
 };
--- src/route.js
+++ src/route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getUserSkills, getSocialLinks, callMailer, getProjectDetails } = require('./controller');
+const { getUserSkills, getSocialLinks, callMailer, getProjectDetails, getCarrierDetails } = require('./controller');
 
 router.get('/portfolio/userskills', getUserSkills);
 router.get('/portfolio/sociallinks', getSocialLinks);
 router.post('/portfolio/sendmail', callMailer);
 router.get('/portfolio/getprojectdetails', getProjectDetails);
+router.get('/portfolio/carrierdetails', getCarrierDetails);
 
 module.exports = router;
